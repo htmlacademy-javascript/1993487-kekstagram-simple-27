2c82d4aec90103b5263d5c6a93de8efa4013ee42
Keks <keks@htmlacademy.ru>
2022-11-02 18:51:39 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/util.js
@@ -0,0 +1,17 @@
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < 0 || min > max || min === max) {
+    return NaN;
+  }
+  const lower = Math.ceil(min);
+  const upper = Math.floor(max);
+  const result = (Math.random() * (upper - lower + 1)) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements [getRandomIntInclusive(0, elements.length - 1)];
+
+const checkStringLength = (string, length) => string.length <= length;
+
+checkStringLength ('', 140);
+
+export {getRandomIntInclusive, getRandomArrayElement};
